603a13ea3dd55d6883b34edabcdb733fe8fc7bbb
zhaojun <zhaojun@yixia.com>
2017-03-02 12:04:44 +0800

    update start.sh and stop.sh

--- lightning-gateway/bin/start.sh
+++ lightning-gateway/bin/start.sh
@@ -8,10 +8,10 @@ nginx_progress=`ps -ef|grep "nginx" |wc -l`
 if [ $nginx_progress -gt 1 ]
 then
     echo "nginx 已经启动,开始重启nginx"
-    $OPENRESTY_INSTALL_PATH/nginx/sbin/nginx  -c  /workspace/click-stream/gate-way/conf/nginx.conf -s reload
+    $OPENRESTY_INSTALL_PATH/nginx/sbin/nginx  -c  /lightning-gateway/conf/nginx.conf -s reload
 else
     echo "nginx 没有启动,开始启动nginx"
-    $OPENRESTY_INSTALL_PATH/nginx/sbin/nginx  -c  /workspace/click-stream/gate-way/conf/nginx.conf
+    $OPENRESTY_INSTALL_PATH/nginx/sbin/nginx  -c  /lightning-gateway/conf/nginx.conf
 fi
 
 nginx_progress=`ps -ef|grep "nginx" |wc -l`
